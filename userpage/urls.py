9a45cef1666249431e1ee4bb476790719b3d51cf
dycraft <dy_private@163.com>
2016-12-08 21:16:12 +0800

    try notice_panel.

--- userpage/urls.py
+++ userpage/urls.py
@@ -6,4 +6,5 @@ from userpage.views import *
 urlpatterns = [
     url(r'^welcome/account_bind/?$', AccountBind.as_view()),
     url(r'^learn/course_list/?$', CourseList.as_view()),
+    url(r'^learn/notice_panel/?$', NoticePanel.as_view())
 ]
--- userpage/views.py
+++ userpage/views.py
@@ -4,6 +4,8 @@ from wechat.models import *
 
 import requests
 import json
+import time
+import datetime
 
 
 class AccountBind(APIView):
@@ -79,3 +81,89 @@ class CourseList(APIView):
             }
         else:
             raise ValidateError('Illegal username.')
+
+
+class NoticePanel(APIView):
+
+    def get(self):
+        # self.check_input('openid')
+        user = User.objects.get(username='2014013434')
+        response = requests.post('http://se.zhuangty.com:8000/curriculum/' + user.username)
+        result = {
+            'student_number': user.student_id,
+            'notices': [],
+            'assignments': [],
+            'files': []
+        }
+        if response.status_code == 200:
+            res = json.loads(response.content.decode())
+            dic = {
+                'read': '已读',
+                'unread': '未读',
+                True: '已批改',
+                False: '未批改'
+            }
+            course_set = []
+            for course in res['classes']:
+                if course['courseid'] in course_set:
+                    continue
+                course_set.append(course['courseid'])
+                response_inform = requests.post('http://se.zhuangty.com:8000/learnhelper/'
+                                                + user.username + '/courses/' + course['courseid']
+                                                + '/notices')
+                if response_inform.status_code == 200:
+                    resp = json.loads(response_inform.content.decode())
+                    for notice in resp['notices']:
+                        inform = {}
+                        inform['title'] = notice['title']
+                        inform['publish_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(notice['publishtime'] / 1000))
+                        inform['state'] = dic[notice['state']]
+                        inform['content'] = notice['content']
+                        result['notices'].append(inform)
+            course_set = []
+            for course in res['classes']:
+                if course['courseid'] in course_set:
+                    continue
+                course_set.append(course['courseid'])
+
+                response_work = requests.post('http://se.zhuangty.com:8000/learnhelper/'
+                                              + user.username + '/courses/' + course['courseid']
+                                              + '/assignments')
+                if response_work.status_code == 200:
+                    resp = json.loads(response_work.content.decode())
+                    for assignment in resp['assignments']:
+                        work = {}
+                        if assignment['duedate'] / 1000 > time.mktime(datetime.datetime.now().timetuple()):
+                            work['processing'] = True
+                        else:
+                            work['processing'] = False
+                        work['title'] = assignment['title']
+                        work['detail'] = assignment['detail']
+                        work['start_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(assignment['startdate'] / 1000))
+                        work['end_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(assignment['duedate'] / 1000))
+                        work['scored'] = dic[assignment['scored']]
+                        work['evaluating_teacher'] = assignment['evaluatingteacher']
+                        work['evaluating_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(assignment['evaluatingdate'] / 1000))
+                        work['comment'] = assignment['comment']
+                        work['grade'] = str(assignment['grade'])
+                        result['assignments'].append(work)
+            course_set = []
+            for course in res['classes']:
+                if course['courseid'] in course_set:
+                    continue
+                course_set.append(course['courseid'])
+                response_work = requests.post('http://se.zhuangty.com:8000/learnhelper/'
+                                              + user.username + '/courses/' + course['courseid']
+                                              + '/documents')
+                if response_work.status_code == 200:
+                    resp = json.loads(response_work.content.decode())
+                    for document in resp['documents']:
+                        file = {}
+                        file['title'] = document['title']
+                        file['explanation'] = document['explanation']
+                        file['updating_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(document['updatingtime'] / 1000))
+                        file['state'] = document['state']
+                        file['size'] = document['size']
+                        file['download_url'] = document['url']
+                        result['files'].append(file)
+        return result
